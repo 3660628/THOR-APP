a9de6f9d8eb8af00333852f8c82e73972bad26ce
danielv775 <danielv775@gmail.com>
2016-01-11 00:12:04 -0500

    V1 build

---
+++ build/THOR APP.build/Debug-iphoneos/THOR APP.build/Objects-normal/arm64/AppDelegate.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/Daniel/Desktop/DJI\ Phantom\ 3/THOR\ APP/THOR\ APP/AppDelegate.mm \
+  /Users/Daniel/Desktop/DJI\ Phantom\ 3/THOR\ APP/THOR\ APP/AppDelegate.h
---
+++ build/THOR APP.build/Debug-iphoneos/THOR APP.build/Objects-normal/arm64/DJIAircraftAnnotation.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/Daniel/Desktop/DJI\ Phantom\ 3/THOR\ APP/THOR\ APP/DJIAircraftAnnotation.m \
+  /Users/Daniel/Desktop/DJI\ Phantom\ 3/THOR\ APP/THOR\ APP/DJIAircraftAnnotation.h \
+  /Users/Daniel/Desktop/DJI\ Phantom\ 3/THOR\ APP/THOR\ APP/DJIAircraftAnnotationView.h
